137c26d8e0ec03fed26a24a1001941672abb91fc
Marisa WONG <9981160@student.swin.edu.au>
2016-05-16 20:51:22 +1000

    IBOutlets and IBActions placed in

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,90 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Marisa WONG on 16/05/2016.
+//  Copyright © 2016 PW. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+  // Mark: Variables
+
+  @IBOutlet weak var calculationLabel: UILabel!
+  
+  // Mark: Buttons
+  @IBAction func clearButton(sender: AnyObject) {
+  }
+  @IBAction func postiveNegativeButton(sender: AnyObject) {
+  }
+  
+  @IBAction func percentageButton(sender: AnyObject) {
+  }
+  
+  @IBAction func divideButton(sender: AnyObject) {
+  }
+  
+  @IBAction func sevenButton(sender: AnyObject) {
+  }
+  
+  @IBAction func eightButton(sender: AnyObject) {
+  }
+  
+  @IBAction func nineButton(sender: AnyObject) {
+  }
+  
+  
+  @IBAction func multiplyButton(sender: AnyObject) {
+  }
+  
+  @IBAction func fourButton(sender: AnyObject) {
+  }
+  
+  @IBAction func fiveButton(sender: AnyObject) {
+  }
+  
+  @IBAction func sixButton(sender: AnyObject) {
+  }
+  
+  @IBAction func minusButton(sender: AnyObject) {
+  }
+  
+  @IBAction func oneButton(sender: AnyObject) {
+  }
+  
+  @IBAction func twoButton(sender: AnyObject) {
+  }
+  
+  @IBAction func threeButton(sender: AnyObject) {
+  }
+  
+  @IBAction func plusButton(sender: AnyObject) {
+  }
+  
+  @IBAction func zeroButton(sender: AnyObject) {
+  }
+  
+  @IBAction func decimalButton(sender: AnyObject) {
+  }
+  
+  @IBAction func calculateButton(sender: AnyObject) {
+  }
+  
+  
+  // Mark:
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
